29491a13f71a3f9073689b5241b28f9cbe632a33
e.davydov <e.davydov@g.nsu.ru>
2025-01-18 14:45:07 +0700

    заменил aws sdk на вторую версию, проблемы с регионом

--- src/main/java/ru/improve/openfy/core/configuration/storage/S3StorageConfig.java
+++ src/main/java/ru/improve/openfy/core/configuration/storage/S3StorageConfig.java
@@ -1,7 +1,18 @@
 package ru.improve.openfy.core.configuration.storage;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.endpoints.Endpoint;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.endpoints.S3EndpointParams;
+import software.amazon.awssdk.services.s3.endpoints.S3EndpointProvider;
 
 @Configuration
 @RequiredArgsConstructor
@@ -9,15 +20,31 @@ public class S3StorageConfig {
 
     private final S3StorageConfigData s3StorageConfigData;
 
-    /*@Bean
-    public AmazonS3 amazonS3() {
-        return AmazonS3ClientBuilder.standard()
-                .withEndpointConfiguration(
-                        new AwsClientBuilder.EndpointConfiguration(
-                                s3StorageConfigData.getServiceEndpoint(), s3StorageConfigData.getSigningRegion()
-                        )
-                )
-                .withCredentials(null)
+    @Value("${openfy.s3.access_key}")
+    private String s3AccessKey;
+
+    @Value("${openfy.s3.private_key}")
+    private String s3SecretKey;
+
+    @Bean
+    public S3Client s3Client() {
+        S3EndpointParams s3EndpointParams = S3EndpointParams.builder()
+                .endpoint(s3StorageConfigData.getServiceEndpoint())
+                .region(Region.of(s3StorageConfigData.getSigningRegion()))
                 .build();
-    }*/
+
+        Endpoint s3Endpoint = S3EndpointProvider
+                .defaultProvider()
+                .resolveEndpoint(s3EndpointParams).join();
+
+
+        AwsCredentials awsCredentials = AwsBasicCredentials.create(s3AccessKey, s3SecretKey);
+        AwsCredentialsProvider awsProvider = StaticCredentialsProvider.create(awsCredentials);
+
+        return S3Client.builder()
+                .credentialsProvider(awsProvider)
+                .endpointOverride(s3Endpoint.url())
+                .forcePathStyle(true)
+                .build();
+    }
 }
--- src/main/java/ru/improve/openfy/core/service/imp/S3ClientServiceImp.java
+++ src/main/java/ru/improve/openfy/core/service/imp/S3ClientServiceImp.java
@@ -1,50 +1,45 @@
 package ru.improve.openfy.core.service.imp;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.improve.openfy.api.dto.upload.UploadTrackRequest;
 import ru.improve.openfy.core.configuration.storage.S3StorageConfigData;
 import ru.improve.openfy.core.service.S3ClientService;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Slf4j
 @Service
 public class S3ClientServiceImp implements S3ClientService {
 
+    private final S3Client s3Client;
+
     private final S3StorageConfigData s3StorageConfigData;
-
-    @Value("${openfy.s3.access_key}")
-    private String s3AccessKey;
-
-    @Value("${openfy.s3.private_key}")
-    private String s3SecretKey;
 
     @Override
     public void saveTrack(UploadTrackRequest uploadTrackRequest) {
         MultipartFile file = uploadTrackRequest.getFile();
 
-        AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
-                    .withEndpointConfiguration(
-                            new AwsClientBuilder.EndpointConfiguration(
-                                    s3StorageConfigData.getServiceEndpoint(), s3StorageConfigData.getSigningRegion()
-                            )
-                    )
-                    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(s3AccessKey, s3SecretKey)))
+        try {
+            Map<String, String> metadata = Map.of(
+                    "size", String.valueOf(file.getBytes().length)
+            );
+
+            PutObjectRequest putRequest = PutObjectRequest.builder()
+                    .bucket(s3StorageConfigData.getMusicBucketName())
+                    .key(uploadTrackRequest.getTrackName())
+                    .metadata(metadata)
                     .build();
 
-        try {
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(file.getBytes().length);
-            amazonS3.putObject(s3StorageConfigData.getMusicBucketName(), uploadTrackRequest.getTrackName(), file.getInputStream(), metadata);
+            RequestBody requestBody = RequestBody.fromBytes(file.getBytes());
+
+        s3Client.putObject(putRequest, requestBody);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
